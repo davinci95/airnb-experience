e43d895ee7cfae78b23c5cd799659142c1f898a2
davinci95 <88544372+davinci95@users.noreply.github.com>
2022-11-28 11:49:10 +0700

    Update Hero.js
    change extent

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
    return (
       <section className="hero">
-         <img src="./images/group.png" className="hero__photo"></img>
+         <img src="./images/group.PNG" className="hero__photo"></img>
          <h1 className="hero__header">Online Experiences</h1>
          <p className="hero__text">
             Join unique interactive activities led by one-of-a-kind hosts—all
